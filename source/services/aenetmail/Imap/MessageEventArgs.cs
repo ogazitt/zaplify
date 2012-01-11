b6279a3b8e72fd623516148832210e754bcc56c6
Omri Gazitt <ogazitt@gmail.com>
2012-01-10 19:48:59 -0800

    removed webservice and nspeex directories

--- source/services/aenetmail/Imap/MessageEventArgs.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace AE.Net.Mail.Imap {
-    public class MessageEventArgs : EventArgs {
-        public int MessageCount { get; set; }
-        internal ImapClient Client { get; set; }
-    }
-}
