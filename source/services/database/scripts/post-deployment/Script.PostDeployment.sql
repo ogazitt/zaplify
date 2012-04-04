f3f774fcbc2faa29118e28bb115c08d5910514f0
Omri Gazitt <ogazitt@gmail.com>
2012-04-04 10:44:42 -0700

    new serviceconfig for production

--- source/services/database/scripts/post-deployment/Script.PostDeployment.sql
+++ source/services/database/scripts/post-deployment/Script.PostDeployment.sql
@@ -140,6 +140,6 @@ GO
 /****** Object:  Table [dbo].[WorkflowTypes]     ******/
 INSERT [dbo].[WorkflowTypes] ([Type], [Definition]) VALUES (N'buy gift', N'{ "Name": "buy gift", "States": [ 
 	{ "Name": "Who is this for?", "Activity": "Get Possible Subjects", "NextState": "Which kind of gift?" },
-	{ "Name": "Which kind of gift?", "Activity": "Get Subject Likes", "NextState": "Useful links" },
-	{ "Name": "Useful links", "Activity": "Get Bing Suggestions", "NextState": null } ] }')
+	{ "Name": "Which kind of gift?", "Activity": "Get Subject Likes", "NextState": "Helpful links" },
+	{ "Name": "Helpful links", "Activity": "Get Bing Suggestions", "NextState": null } ] }')
 GO
