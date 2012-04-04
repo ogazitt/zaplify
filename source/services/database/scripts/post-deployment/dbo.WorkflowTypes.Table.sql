883c24c7f7802f38df043084060d3275ba054bea
Omri Gazitt <ogazitt@gmail.com>
2012-04-04 11:23:19 -0700

    refactored the post deployment script by splitting up into different tables

---
+++ source/services/database/scripts/post-deployment/dbo.WorkflowTypes.Table.sql
@@ -0,0 +1,6 @@
+﻿/****** Object:  Table [dbo].[WorkflowTypes]     ******/
+INSERT [dbo].[WorkflowTypes] ([Type], [Definition]) VALUES (N'buy gift', N'{ "Name": "buy gift", "States": [ 
+	{ "Name": "Who is this for?", "Activity": "Get Possible Subjects", "NextState": "Which kind of gift?" },
+	{ "Name": "Which kind of gift?", "Activity": "Get Subject Likes", "NextState": "Helpful links" },
+	{ "Name": "Helpful links", "Activity": "Get Bing Suggestions", "NextState": null } ] }')
+GO
